1eb3f86488a0c08f8b9079e7b6c004228fbf7dff
WTheodoro <theodoro.walison@gmail.com>
2021-01-17 16:04:52 -0300

    exercicio 06 local storage

---
+++ aula_05/lista/app.js
@@ -0,0 +1,19 @@
+const bancoLocal = new BancoLocal()
+const lista = new Lista()
+
+
+lista.botaoAdicionar.addEventListener("click", () => {
+  bancoLocal.gravar(lista.inputItem.value)
+  bancoLocal.ler()
+  lista.adicionarNaLista(bancoLocal.minhaLista)
+})
+
+lista.botaoApagar.addEventListener("click", () => {
+  localStorage.clear()
+  lista.lista.innerHTML = ''
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+  bancoLocal.ler()
+  lista.adicionarNaLista(bancoLocal.minhaLista)
+})
---
+++ aula_05/lista/lista.js
@@ -0,0 +1,31 @@
+class Lista {
+  constructor() {
+    this.inputItem = document.querySelector("#item")
+    this.botaoAdicionar = document.querySelector("#adicionar")
+    this.lista = document.querySelector("#lista")
+    this.botaoApagar = document.querySelector("#apagar")
+  }
+
+  apagarLista() {
+    localStorage.clear()
+    // lista.innerHTML = ''
+  }
+
+  adicionarNaLista(minhaLista) {
+    console.log(typeof minhaLista)
+    console.log(minhaLista)
+    let listaHTML = ''
+
+    minhaLista.map( (params) => {
+      listaHTML += 
+      `
+      <li>
+          ${params}
+      </li>
+      `
+    })
+
+    this.lista.innerHTML = listaHTML
+  }
+
+}
