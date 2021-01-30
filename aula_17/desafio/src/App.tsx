3fa601d4b5e9e665f0f5bd93c4273d7ed4deb9d4
WTheodoro <theodoro.walison@gmail.com>
2021-01-29 21:20:11 -0300

    desafio sula 29/01

---
+++ aula_17/desafio/src/App.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+import { UserType, PostType } from './types/UserType';
+
+function App() {
+
+  // const [user, setUser] = useState<any>()
+  const [user, setUser] = useState<UserType[]>()
+  const [id, setId] = useState<number>()
+  const [post, setPost] = useState<any>()
+
+  useEffect(() =>{
+    api.get(``)
+    .then(response => setUser(response.data))
+  }, [])
+
+  useEffect(() => {
+    api.get(`/${id}/posts`)
+    .then(response => setPost(response.data))
+  }, [id])
+  
+  return (
+    <div className="App">
+
+      {
+        user?.map((i: UserType) => (
+          <div key={i.id}>
+            <p onClick={() => setId(i.id)}>{i.name}</p>
+            <p>{i.id}</p>
+          </div>
+        ))
+      }
+      
+      {
+        post?.map((i: PostType) => (
+          <>
+            <div key={i.id}>
+              <p><strong>{i.title}</strong></p>
+              <p>{i.body}</p>
+            </div>
+          </>
+        ))
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ aula_17/desafio/src/types/UserType.ts
@@ -0,0 +1,10 @@
+export interface UserType {
+    id: number
+    name: string
+}
+
+export interface PostType {
+    id: number
+    title: string
+    body: string
+}
