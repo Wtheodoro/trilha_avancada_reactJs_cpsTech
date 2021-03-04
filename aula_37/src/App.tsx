1408a7da2524cbb727b535fec54af415a3e7ebde
WTheodoro <theodoro.walison@gmail.com>
2021-03-03 22:51:46 -0300

    clean

--- aula_37/src/App.tsx
+++ aula_37/src/App.tsx
@@ -1,63 +1,11 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import { useForm } from 'react-hook-form'
-import { RadioGroup, FormControlLabel, Radio, FormLabel, Button } from '@material-ui/core';
 import './App.css';
 import Form from './components/Form';
 
 function App() {
-  const { register, handleSubmit, errors} = useForm()
-
-  // const onSubmit = (params: any) => {
-  //   console.log(params)
-  // }
-
   return (
     <div className="App">
       <Form />
-      {/* <form action="" onChange={handleSubmit(onSubmit)}>
-        <TextField
-          id="outlined-nomeText"
-          label="Nome"
-          helperText="Campo obrigatório*"
-          variant="outlined"
-          name="nome"
-          inputRef={register}
-        />
-        <TextField
-          id="outlined-cidadeText"
-          label="Cidade"
-          helperText="Campo obrigatório*"
-          variant="outlined"
-          name="cidade"
-          inputRef={register}
-        />
-        <TextField
-          id="outlined-emailText"
-          label="E-mail"
-          helperText="Campo obrigatório*"
-          variant="outlined"
-          name="email"
-          inputRef={register}
-        />
-        <TextField
-          id="outlined-idadeText"
-          label="idade"
-          helperText="Campo obrigatório*"
-          variant="outlined"
-          name="idade"
-          inputRef={register}
-        />
-          <FormLabel component="legend">Estado Civil</FormLabel>
-          <RadioGroup aria-label="gender" name="estado-civil">
-            <FormControlLabel value="Solteire" control={<Radio />} label="Solteire" />
-            <FormControlLabel value="Casade" control={<Radio />} label="Casade" />
-            <FormControlLabel value="Outro" control={<Radio />} label="Outro" />
-          </RadioGroup>
-        <Button variant="contained" color="secondary" type="submit">
-          Submeter
-        </Button>
-      </form> */}
     </div>
   );
 }
--- aula_38/src/components/Lista/Lista.test.tsx
+++ aula_38/src/components/Lista/Lista.test.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import Lista from './index'
+import '@testing-library/jest-dom'
+
 
 test('Existe o testo?', () => {
     render(<Lista />)
     const  list = screen.getByTestId('lista')
-    expect(list).not.toBeEmptyDOMElement
+    expect(list).not.toBeEmptyDOMElement()
 })
